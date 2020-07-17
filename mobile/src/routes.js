d5c48efbb8e5ad0635d46c74a6128357f64118b3
Lucas Ferreira <blooodcf@gmail.com>
2020-07-17 09:17:18 -0400

    Feat: Routes setup :sparkles:

---
+++ mobile/src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Login from "./screens/Login";
+
+const Routes = () => (
+  <NavigationContainer>
+    <Login />
+  </NavigationContainer>
+);
+
+export default Routes;
